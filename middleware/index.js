f92e88a0ef3d77455a72ac4c277a45c561f71fbd
Dan Thareja <danthareja@gmail.com>
2018-01-14 22:41:34 -0700

    Remove unnecessary log

--- middleware/index.js
+++ middleware/index.js
@@ -1,14 +1,13 @@
 module.exports.validateAPIKey = async (req, res, next) => {
-  const secret = req.headers['x-api-key'];
+  const key = req.headers['x-api-key'];
 
-  console.log(secret, process.env.API_KEY)
-  if (!secret) {
+  if (!key) {
     return res.status(401).json({
       error: 'Missing X-API-Key header'
     })
   }
 
-  if (secret !== process.env.API_KEY) {
+  if (key !== process.env.API_KEY) {
     return res.status(401).json({
       error: 'Invalid X-API-Key header'
     })
