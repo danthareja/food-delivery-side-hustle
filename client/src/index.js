f12af4faed7333d83017046f7709f600ccd7af75
Dan Thareja <danthareja@gmail.com>
2018-01-29 10:31:11 -0700

    Restructure client

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import 'whatwg-fetch'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
 
 import './index.css'
